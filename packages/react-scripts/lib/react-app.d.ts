03224b3bd50ddb3e9f136effa3b25aeac701fe46
Chnapy <richardhaddad@hotmail.fr>
2020-04-08 11:57:21 +0200

    Fix json module typing

--- packages/react-scripts/lib/react-app.d.ts
+++ packages/react-scripts/lib/react-app.d.ts
@@ -64,3 +64,8 @@ declare module '*.module.sass' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare module '*.json' {
+  const src: string;
+  export default src;
+}
